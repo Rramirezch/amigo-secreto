2bef7e1fc42075cb9d800c68dc6febac705f59e5
Rramirezch <rodrigora05@yahoo.com>
2025-08-07 08:58:19 -0500

    se crean primeras funciones del juego

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    // Verifica si el nombre ingresado no está vacío
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+    }
+  // Agrega el nombre del amigo al arreglo "amigos"
+        amigos.push(nombre);
+        console.log(amigos);
+    mostrarAmigos()
+    // Limpia el input después de agregar el amigo
+    limpiarInput();
+}
+
+function limpiarInput() {
+    document.getElementById("amigo").value = "";
+}
+
+
+function mostrarAmigos() {
+   //adiciona al index.html la lista de amigos
+   let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach(function(amigo) {
+         let li = document.createElement("li");
+         li.textContent = amigo;
+         lista.appendChild(li);
+    });
+}
